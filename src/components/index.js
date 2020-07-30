64d960f7d67356b4d4a3f334b71f4531db2b925e
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 22:08:17 +0300

    refactor: updated default exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar';
-export { default as Button } from './Button/Button';
 export { default as QuestionBlock } from './QuestionBlock/QuestionBlock';
+export { default as AnswersList } from './AnswersList/AnswersList';
