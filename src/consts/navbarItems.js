530c2497ffedda95fcc70bbf41fdf68d041e6631
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 17:45:42 +0300

    feat: added data file for header

---
+++ src/consts/navbarItems.js
@@ -0,0 +1,32 @@
+export const navbarItems = [
+  {
+    name: 'Перелётные',
+    section: 'migratory',
+    id: 1,
+  },
+  {
+    name: 'Воробьиные',
+    section: 'sparrow',
+    id: 2,
+  },
+  {
+    name: 'Лесные птицы',
+    section: 'forest',
+    id: 3,
+  },
+  {
+    name: 'Певчие птицы',
+    section: 'songbirds',
+    id: 4,
+  },
+  {
+    name: 'Хищные птицы',
+    section: 'predatory',
+    id: 5,
+  },
+  {
+    name: 'Морские птицы',
+    section: 'marine',
+    id: 6,
+  },
+];
