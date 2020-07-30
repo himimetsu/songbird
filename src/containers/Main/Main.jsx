f375bb9fc5f5904eba5c065abefc10928fbe4302
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 21:33:53 +0300

    feat: added two question-blocks and button

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Main.scss';
+import { QuestionBlock, Button } from '../../components';
 
 const Main = () => {
   return (
     <main className='site-main'>
+      <QuestionBlock type='issue' element='site-main__issue' />
+      <QuestionBlock type='description' element='site-main__description' />
+      <Button
+        classbtn='game-btn'
+        type='button'
+        text={status === 'game' ? 'Next Level' : 'Start game'}
+      />
     </main>
   );
 };
