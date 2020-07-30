c06bcb96c7993d8becf6f3553fd2b546ed4208d5
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 17:46:42 +0300

    feat: added app logo and current score

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png';
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className='site-header'>
+      <div className='site-header__title site-info'>
+        <div className='site-info__logo logo-content'>
+          <img className='logo-content__photo' src={logo} alt="logo" />
+          <div className='logo-content__text' data-position='1'>Song</div>
+          <div className='logo-content__text' data-position='2'>Bird</div>
+        </div>
+
+        <div className='site-info__score'>
+          Score: 123
+        </div>
+      </div>
     </header>
   );
 };
