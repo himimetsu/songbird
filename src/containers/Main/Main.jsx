3224c25fac22f5b154c30431d0aca2230a1ac724
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 22:11:41 +0300

    refactor: updated imports and added answers list

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Main.scss';
-import { QuestionBlock, Button } from '../../components';
+import { QuestionBlock, AnswersList } from '../../components';
+import { Button } from '../../UI';
 
 const Main = () => {
   return (
     <main className='site-main'>
       <QuestionBlock type='issue' element='site-main__issue' />
+      <AnswersList element='site-main__answers' />
       <QuestionBlock type='description' element='site-main__description' />
       <Button
         classbtn='game-btn'
