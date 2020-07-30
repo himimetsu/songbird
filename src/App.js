be02142fc7018dcf932f63ab4e46e4f3c25be577
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 15:37:55 +0300

    feat: wrapped the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from './hoc/Layout/Layout';
 
 const App = () => {
   return (
-    <div className='app'>
-      hello world
-    </div>
+    <Layout>
+      
+    </Layout>
   );
 };
 
