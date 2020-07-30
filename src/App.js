200d4791433f1bec6a95391a1a308748cce360a8
Vladislav Ivanov <himametsu@gmail.com>
2020-07-30 15:54:51 +0300

    feat: implemented render of containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import { Header, Main, Footer } from './containers';
 
 const App = () => {
   return (
     <Layout>
-      
+      <Header/>
+      <Main />
+      <Footer />
     </Layout>
   );
 };
