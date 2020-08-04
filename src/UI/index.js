dbf29077bb8184d58bc2331bf06b5b09362eb5ab
Vladislav Ivanov <himametsu@gmail.com>
2020-08-04 22:57:03 +0300

    feat: added default react-player and default-text export

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,1 +1,3 @@
 export { default as Button } from '../UI/Button/Button';
+export { default as ReactPlayer } from '../UI/ReactPlayer/ReactPlayer';
+export { default as DefaultText } from '../UI/DefaultText/DefaultText';
