ccb992aba82c881c194c4831d15b6c0a1b7a8c55
Vladislav Ivanov <himametsu@gmail.com>
2020-08-04 22:55:00 +0300

    feat: implemented default text component

---
+++ src/UI/DefaultText/DefaultText.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DefaultText.scss';
+
+const DefaultText = () => {
+  return (
+    <div className='default-text'>
+      <span>Послушайте плеер.</span> <br/>
+      <span>Выберите птицу из списка.</span>
+    </div>
+  );
+};
+
+export default DefaultText;
