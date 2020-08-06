330782dd700a652a8db58fe688445ed5bc7d42b3
Vladislav Ivanov <himametsu@gmail.com>
2020-08-07 00:00:52 +0300

    refactor: deleted useless props

--- src/components/Navbar/NavbarItem/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem/NavbarItem.jsx
@@ -3,18 +3,13 @@ import { MainContext } from '../../../context/MainContext';
 import './NavbarItem.scss';
 
 const NavbarItem = ({ name, section }) => {
-  const { currentSection, changeCurrentSection, status } = useContext(MainContext);
+  const { currentSection } = useContext(MainContext);
   const classes = ['nav-bar__link'];
-
-  const navItemHandler = () => status === 'warmup' && changeCurrentSection(section);
 
   section === currentSection && classes.push('nav-bar__link--active');
 
   return (
-    <a
-      className={classes.join(' ')}
-      onClick={() => navItemHandler()}
-    >
+    <a className={classes.join(' ')}>
       {name}
     </a>
   );
