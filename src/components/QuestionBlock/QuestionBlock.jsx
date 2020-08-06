d5ce1f648407f9d3a6fc0f900301662410cc7f19
Vladislav Ivanov <himametsu@gmail.com>
2020-08-07 00:03:51 +0300

    refactor: changed the used database to your own

--- src/components/QuestionBlock/QuestionBlock.jsx
+++ src/components/QuestionBlock/QuestionBlock.jsx
@@ -1,42 +1,51 @@
 import React, { useContext } from 'react';
-import { birdsData } from '../../consts/birdsData';
+import defaultBird from '../../assets/images/defaultBird.jpg';
+import { data } from '../../consts/data';
 import { GameContext } from '../../context/GameContext';
 import { MainContext } from '../../context/MainContext';
+import { ReactPlayer } from '../../UI';
 import './QuestionBlock.scss';
 
 const QuestionBlock = ({ element, type }) => {
-  const { currentId, isRight } = useContext(GameContext);
+  const { currentId, isRight, forcedPause } = useContext(GameContext);
   const { selectedBird, currentSection, status } = useContext(MainContext);
 
   return (
-    <div className={`${element} player-wrapper`}>
+    <div className={`${element} player-wrapper`} data-type={type}>
       <div className='player-wrapper__photo'>
         <img
           className='player-wrapper__img'
           alt="bird"
           src={type === 'description'
-            ? selectedBird.image
-            : isRight && birdsData[currentSection][currentId].image
+            ? selectedBird.image ? selectedBird.image : defaultBird
+            : isRight ? data[currentSection][currentId].image : defaultBird
           }
         />
       </div>
       <div className='player-wrapper__content content-info'>
         <div className='content-info__name'>
+
           {type === 'description'
-            ? selectedBird.name ? selectedBird.name : 'Название птицы'
-            : isRight ? birdsData[currentSection][currentId].name : '***'
+            ? selectedBird.name && selectedBird.name
+            : isRight ? data[currentSection][currentId].name : '*****'
           }
         </div>
+
         {type === 'description' && 
           <div className='content-info__latin'>
-            {selectedBird.species ? selectedBird.species : 'Латинское название'}
+            {selectedBird.species && selectedBird.species}
           </div>
         }
+
         <div className='content-info__audioplayer'>
-          <audio controls className='react-audio-player'
-          src={type === 'description'
-            ? selectedBird.audio
-            : status === 'game' && birdsData[currentSection][currentId].audio}
+          <ReactPlayer
+            forcedPause={forcedPause}
+            birdsData={data}
+            type={type}
+            selectedBird={selectedBird}
+            status={status}
+            currentId={currentId}
+            currentSection={currentSection}
           />
         </div>
       </div>
