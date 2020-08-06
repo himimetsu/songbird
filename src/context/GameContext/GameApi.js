07f44fa415c513035cbcb4e1c6bcf5a2839c108d
Vladislav Ivanov <himametsu@gmail.com>
2020-08-07 00:03:13 +0300

    refactor: updated game api

--- src/context/GameContext/GameApi.js
+++ src/context/GameContext/GameApi.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { GameContext } from './index';
+import { getRandomInRange } from '../../utils/getRandomInRange';
 
 const GameApi = ({ children }) => {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ const GameApi = ({ children }) => {
   const [isRight, setIsRight] = useState(false);
   const [currentId, setCurrentId] = useState();
   const [rightAnswers, setRightAnswers] = useState(0);
+  const [forcedPause, setForcedPause] = useState(false);
 
   const changeCurrentId = (value) => setCurrentId(value);
   const changeCount = (value) => setCount(value);
@@ -15,16 +17,15 @@ const GameApi = ({ children }) => {
   const changeWrong = (value) => setWrong(value);
   const changeIsRight = (value) => setIsRight(value);
   const changeRightAnswers = (value) => setRightAnswers(value);
+  const changeForcedPause = (value) => setForcedPause(value);
 
   useEffect(() => {
-    if (rightAnswers === 6 || score === 30) {
-      console.log('game over');
-    }
-  }, [rightAnswers, score]);
+    setCurrentId(getRandomInRange(0, 5));
+  }, []);
 
   return (
     <GameContext.Provider
-      value={{ score, changeScore, wrong, isRight, currentId, changeCurrentId, count, changeCount, changeWrong, changeIsRight, rightAnswers, changeRightAnswers }}
+      value={{ score, changeScore, wrong, isRight, currentId, changeCurrentId, count, changeCount, changeWrong, changeIsRight, rightAnswers, changeRightAnswers, forcedPause, changeForcedPause }}
     >
       {children}
     </GameContext.Provider>
