eb6c8dc392689643399a8e4fa73293ca04f03eb5
Vladislav Ivanov <himametsu@gmail.com>
2020-08-06 23:42:03 +0300

    feat: added a hint for the correct answer

--- src/utils/getRandomInRange.js
+++ src/utils/getRandomInRange.js
@@ -1,5 +1,5 @@
 export const getRandomInRange = (min, max) => {
   const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log('Правильный ответ:', randomValue);
+  console.log('Правильный ответ:', randomValue + 1);
   return randomValue;
 };
