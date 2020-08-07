29e4ad16131dd6a974e6c2d27868cacb077e2a46
Vladislav Ivanov <himametsu@gmail.com>
2020-08-07 17:13:14 +0300

    refactor: deleted console log

--- src/utils/getRandomInRange.js
+++ src/utils/getRandomInRange.js
@@ -1,5 +1,4 @@
 export const getRandomInRange = (min, max) => {
   const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log('Правильный ответ:', randomValue + 1);
   return randomValue;
 };
